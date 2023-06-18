ea57286989c74b73c835f503ca12d554fb0a0996
Aaron Fleshner <adfleshner@gmail.com>
2023-06-18 18:08:09 -0400

    Added Sharing VB Delegate and ktlint
    Added sharing the current gif url
    Added ViewBinding Delegate for Fragment and Activity
    Added Ktlint to the project.

--- app/src/main/java/io/shits/and/gigs/randomcodinglove/MainActivity.kt
+++ app/src/main/java/io/shits/and/gigs/randomcodinglove/MainActivity.kt
@@ -1,39 +1,22 @@
 package io.shits.and.gigs.randomcodinglove
 
-import Love
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.viewModels
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
-import androidx.lifecycle.flowWithLifecycle
-import androidx.lifecycle.lifecycleScope
-import com.bumptech.glide.Glide
-import com.flesh.dataaboutapp.dataaboutapp.AboutAppListener
-import com.flesh.dataaboutapp.dataaboutapp.DataAboutAppDialogFragment
-import com.flesh.dataaboutapp.dataaboutapp.DataAboutAppRepository
-import com.flesh.dataaboutapp.dataaboutapp.bindShake
-import com.squareup.seismic.ShakeDetector
+import io.shits.and.gigs.randomcodinglove.base.BaseActivity
 import io.shits.and.gigs.randomcodinglove.databinding.ActivityMainBinding
-import io.shits.and.gigs.randomcodinglove.viewmodels.MoreLoveViewModel
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
+import io.shits.and.gigs.randomcodinglove.utils.viewBinding
 
 class MainActivity : BaseActivity() {
 
-    private val _binding: ActivityMainBinding by lazy {
-        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-        binding
-    }
+    private val _binding by viewBinding(ActivityMainBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(_binding.root)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.content, RandomLoveFragment(), RandomLoveFragment.TAG).commit()
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.content, RandomLoveFragment.newInstance(), RandomLoveFragment.TAG)
+                .commit()
         }
     }
-
 }
-
